671576f329a659c246a115fd3e367e4c8b749333
Anton Grebinnyi <grants404@outlook.com>
2024-07-18 16:11:49 +0800

    Finishing up the basic functionality
    The program can now load and plot the speed/distance graph for two drivers

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from PyQt6 import QtCore, QtWidgets, QtGui
 import sys
 import fastf1
+import fastf1.plotting
+import matplotlib.pyplot as plt
 import numpy as np
 
 
@@ -20,6 +22,7 @@ class App(QtWidgets.QWidget):
         self.init_ui()
 
     def init_ui(self):
+        fastf1.plotting.setup_mpl(misc_mpl_mods=False)
         self.setGeometry(200, 160, 400, 300)
         font = QtGui.QFont("Halvetica", 12)
 
@@ -35,6 +38,7 @@ class App(QtWidgets.QWidget):
 
         self.race_options.resize(200, 25)
         self.race_options.move(160, 50)
+        self.race_options.activated.connect(self.on_race_chosen)  # Connect the activated signal to a slot
 
         self.driver1_text.setFont(font)
         self.driver1_text.move(50, 100)
@@ -47,6 +51,7 @@ class App(QtWidgets.QWidget):
 
         self.query_button.resize(100, 50)
         self.query_button.move(150, 200)
+        self.query_button.clicked.connect(self.plot)
 
         self.show()
 
@@ -61,6 +66,35 @@ class App(QtWidgets.QWidget):
         for i in range(countries.size):
             options.append(countries[i] + ', ' + locations[i])
         self.race_options.addItems(options)
+
+    @QtCore.pyqtSlot()
+    def on_race_chosen(self):
+        self.session = fastf1.get_session(int(self.season.text()), self.race_options.currentText(), 'Q')
+        self.session.load()
+        drivers = self.session.drivers
+        self.driver1.addItems(drivers)
+        self.driver2.addItems(drivers)
+
+    @QtCore.pyqtSlot()
+    def plot(self):
+        driver1_lap = self.session.laps.pick_driver(self.driver1.currentText()).pick_fastest()
+        driver2_lap = self.session.laps.pick_driver(self.driver2.currentText()).pick_fastest()
+        driver1_tel = driver1_lap.get_car_data().add_distance()
+        driver2_tel = driver2_lap.get_car_data().add_distance()
+
+        fig, ax = plt.subplots()
+        ax.plot(driver1_tel['Distance'], driver1_tel['Speed'], color="green", label=self.session.get_driver(self.driver1.currentText())['Abbreviation'])
+        ax.plot(driver2_tel['Distance'], driver2_tel['Speed'], color="red", label=self.session.get_driver(self.driver2.currentText())['Abbreviation'])
+
+        ax.set_xlabel('Distance in m')
+        ax.set_ylabel('Speed in km/h')
+
+        ax.legend()
+        plt.suptitle(f"Fastest Lap Comparison \n "
+                     f"{self.session.event['EventName']} {self.session.event.year} Qualifying")
+
+        plt.show()
+
 
 
 if __name__ == "__main__":
